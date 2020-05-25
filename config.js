1f6ce479612edc1b1388cc9c2ccbdd4088cc9de8
Jose Flores <josecruzflores.fl@gmail.com>
2020-05-25 18:49:21 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/dungeonsdb';
 exports.PORT = process.env.PORT || 8080;
-exports.API_SECRET = 'secret';
-exports.HASHING_ROUNDS = 10;
+exports.API_SECRET = process.env.API_SECRET || 'secret';
+exports.HASHING_ROUNDS = process.env.HASHING_ROUNDS || 10;
